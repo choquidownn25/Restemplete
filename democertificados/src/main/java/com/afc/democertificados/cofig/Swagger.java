a7357adb2674bfc8b6d575fb1ef04bf584ff7493
Choquidownn25 <jose_sarria2003@yahoo.com.mx>
2023-06-12 11:05:00 -0500

    Swagger 3.0

--- democertificados/src/main/java/com/afc/democertificados/cofig/Swagger.java
+++ democertificados/src/main/java/com/afc/democertificados/cofig/Swagger.java
@@ -3,50 +3,66 @@
  */
 package com.afc.democertificados.cofig;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 /**
  * @author Madre Hermosa
  *
  */
 @Configuration
-@EnableSwagger2
 public class Swagger {
 	
-	@Bean
-	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.afc.democertificados.controller"))
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(getApiInfo());
-	}
-	
-	private ApiInfo getApiInfo() {
-		return new ApiInfo(
-				"Order Service API",
-				"Order Service API Description",
-				"1.0",
-				"http://codmind.com/terms",
-				new Contact("Codmind", "https://codmind.com", "apis@codmind.com"),
-				"LICENSE",
-				"LICENSE URL",
-				Collections.emptyList());
-	}
+	 public static final String AUTHORIZATION_HEADER = "Authorization";
+
+	    private ApiInfo apiInfo() {
+	        return new ApiInfo("My REST API- Empresa Talataa",
+	                "Some custom description of API.",
+	                "1.0",
+	                "Terms of service",
+	                new Contact("Jose Antonio Sarria", "www.baeldung.com", "jose_sarria2003@yahoo.com.mx"),
+	                "License of API",
+	                "API license URL",
+	                Collections.emptyList());
+	    }
+
+	    @Bean
+	    public Docket api() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .apiInfo(apiInfo())
+	                .securityContexts(Arrays.asList(securityContext()))
+	                .securitySchemes(Arrays.asList(apiKey()))
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("org.example.controller"))
+	                .paths(PathSelectors.any())
+	                .build();
+	    }
+
+	    private ApiKey apiKey() {
+	        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+	    }
+
+	    private SecurityContext securityContext() {
+	        return SecurityContext.builder()
+	                .securityReferences(defaultAuth())
+	                .build();
+	    }
+
+	    List<SecurityReference> defaultAuth() {
+	        AuthorizationScope authorizationScope
+	                = new AuthorizationScope("global", "accessEverything");
+	        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+	        authorizationScopes[0] = authorizationScope;
+	        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+	    }
 	
 }
